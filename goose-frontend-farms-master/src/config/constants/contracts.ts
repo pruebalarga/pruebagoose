02ccc549d16a188c5e098a3f08798f95789d7360
pruebalarga <97545520+pruebalarga@users.noreply.github.com>
2022-01-11 13:11:23 -0500

    initial

---
+++ goose-frontend-farms-master/src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    250: '0x2C3A91137DF0B911f2e0Da344aC32fA7bDcb1db4',
+    97: '',
+  },
+  masterChef: {
+    250: '0xF70dd54B26A937362b06479A964A41d04ddE5C4b',
+    97: '',
+  },
+  wbnb: {
+    250: '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83',
+    97: '',
+  },
+  lottery: {
+    250: '',
+    97: '',
+  },
+  lotteryNFT: {
+    250: '',
+    97: '',
+  },
+  mulltiCall: {
+    250:  '0xf37dA3428F0C8191149D553FBb218FA8a1Ab2f0f',
+    97:  '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    250: '0x04068da6c83afcfa0e13ba15a6696662335d5b75',
+    97: '',
+  },
+}
